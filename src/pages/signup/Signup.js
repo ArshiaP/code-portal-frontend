0d090e73c02333c7b1999669b49d6a5fcfe4ee3b
ArshiaP <arshiaputhran08@gmail.com>
2023-02-25 22:55:01 +0530

    signup

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,21 +1,49 @@
 import React from 'react'
-
+import { Form, Container } from 'semantic-ui-react'
+import './signup.css'
 
 function Signup() {
   return (
     <div className='signup-container'>
       <div className="left">
-        <div className="title">Sign In</div>
+        <div className="child">
+          <div className="title">Sign In</div>
+          <div className="form">
+            <Form>
+              <Form.Field>
+                <label>Email ID</label>
+                <input placeholder='Email ID' />
+              </Form.Field>
+              <Form.Field>
+                <label>Username</label>
+                <input placeholder='Username' />
+              </Form.Field>
+              <Form.Field>
+                <label>Password</label>
+                <input type={'password'} placeholder='Password' />
+              </Form.Field>
+              <Form.Field>
+                <label>Confirm Password</label>
+                <input type={'password'} placeholder='Confirm Password' />
+              </Form.Field>
+              <div className="button">
+                Sign Up
+              </div>
+            </Form>
+          </div>
+        </div>
       </div>
       <div className="right">
-        <div className="typing">
-          printf(“Hello World”);
-        </div>
-        <div className="question">
-          Do not have an account?
-        </div>
-        <div className="signuplink">
-          Sign Up
+        <div className="child">
+          <div className="typing">
+            printf(“Hello World”);
+          </div>
+          <div className="question">
+            Already have an account?
+          </div>
+          <div className="button">
+            Sign In
+          </div>
         </div>
       </div>
     </div>
